dc1a6e50ff189b6613e1021f9a6c15b427230b4e
arman-lukac <arman@vinylmeplease.com>
2025-03-16 01:23:45 +0100

    signup dto

--- src/auth/dto/signup.dto/signup.dto.ts
+++ src/auth/dto/signup.dto/signup.dto.ts
@@ -1,16 +1,28 @@
 import { IsEmail, IsString, MinLength, Matches } from 'class-validator';
 
 export class SignupDto {
-  @IsEmail()
+  @IsEmail({}, { message: 'Invalid email format' })
   email: string;
 
   @IsString()
-  @MinLength(8)
-  @Matches(
-    /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-    {
-      message: 'Password too weak',
-    },
-  )
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @Matches(/[A-Z]/, {
+    message: 'Password must contain at least one uppercase letter',
+  })
+  @Matches(/[a-z]/, {
+    message: 'Password must contain at least one lowercase letter',
+  })
+  @Matches(/\d/, { message: 'Password must contain at least one number' })
+  @Matches(/[@$!%*?&]/, {
+    message: 'Password must contain at least one special character (@$!%*?&)',
+  })
   password: string;
+
+  @IsString()
+  @MinLength(3, { message: 'First name must be at least 3 characters long' })
+  first_name: string;
+
+  @IsString()
+  @MinLength(3, { message: 'Last name must be at least 3 characters long' })
+  last_name: string;
 }
